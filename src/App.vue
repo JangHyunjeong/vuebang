f249e27be4d5bcc41cc749e3fe978400a3def826
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-09 16:32:19 +0900

    12.상품정렬과 데이터 원본 보존 : sort, [...array]

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <h2>9. v-model</h2>
+    <h2>12. 상품정렬기능과 데이터 원본 보존</h2>
     <div class="menu">
         <a v-for="(item, idx) in menuItem" :key="idx">{{ item }}</a>
     </div>
   </div>
 
   <DiscountBanner/>
+
+  <button @click="sortPriceLowToHigh">가격낮은순정렬</button>
+  <button @click="sortPriceHighToLow">가격높은순정렬</button>
+  <button @click="sortTitle">상품명가나다순</button>
+  <button @click="sortBack">되돌리기</button>
   
   <PopupModal @closeModal="closeModal" :productList="productList" :modalIdx="modalIdx" :modalIsOepn="modalIsOepn" :closeModal="closeModal"/>
 
@@ -29,6 +34,9 @@ export default {
   data() {
     return {
       modalIsOepn: false,
+      // array, object데이터의 각각 별개 사본을 만드려면
+      // [...array자료]
+      productListOriginal: [...productListData],
       productList: productListData,
       menuItem: ['Home', 'About', 'Products'],
       modalIdx: 0,
@@ -40,13 +48,57 @@ export default {
       this.productList[idx].reported++;
     },
 
-    openModal(idx) {
+    openModal(idx){
       this.modalIsOepn = true;
       this.modalIdx = idx;
     },
 
     closeModal(){
       this.modalIsOepn = false;
+    },
+
+    sortPriceLowToHigh(){
+      // var array = [2,5,3];
+      // sort() 는 사실 문자 정렬,ㄱㄴㄷ순.
+
+      // 숫자 정렬은 다음과 같이
+      // console.log(array.sort(function(a, b){
+      //   return a - b;  // 음수면 a를 왼쪽으로 보내주세요
+      // }))
+
+      // 요즘은 sort 잘 안씀 - 원본 데이터가 변경되니까, map , filter 와 같은 메소드 쓰는게 나음
+      // 아니면 sort를 쓰더라도 [...array] 사용하여 원본을 저장할 수 있음
+
+      this.productList.sort(function(a,b){
+        return a.price - b.price;
+      });
+    },
+
+    sortPriceHighToLow(){
+      this.productList.sort(function(a,b){
+        return b.price - a.price;
+      })
+    },
+
+    sortTitle(){
+      this.productList.sort(function(a,b){
+         let x = a.title;
+         let y = b.title;
+
+        if(x < y){
+          return -1;
+        }
+        if(x > y) {
+          return 1;
+        }
+        return 0;
+      });
+    },
+
+    // 등호로 array를 집어넣으면 왼쪽 오른쪽 값을 공유해주세요~ 임
+    // 그래서 [...array]자료 (카피본)을 집어넣어주세요
+    sortBack(){
+      this.productList = [...this.productListOriginal];
     }
   },
 
